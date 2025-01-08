b5d4968604055b9adc57037c29a533f8c098c37f
vinyesm <vinyes.marina@gmail.com>
2025-01-08 12:08:00 +0100

    edit a simple pyspark example

--- examples/pyspark_example.py
+++ examples/pyspark_example.py
@@ -3,10 +3,6 @@ import shutil
 import os
 from pyspark.sql import SparkSession  # pylint: disable=import-outside-toplevel
 
-output_dir = os.path.abspath("bench")
-
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir)
 
 spark = (
     SparkSession.builder.config("spark.driver.memory", "16G").master("local[16]").appName("spark-stats").getOrCreate()
@@ -16,7 +12,7 @@ download(
     processes_count=16,
     thread_count=32,
     url_list="s3://my-numina/text5B/part-00080-dc7779ff-a280-46f5-8cd8-bf64283145c8-c000.snappy.parquet",
-    output_folder="s3://my-numina/pdf-math-bench,
+    output_folder="s3://my-numina/bench",
     output_format="files",
     input_format="parquet",
     url_col="url",
