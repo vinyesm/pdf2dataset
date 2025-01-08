5a461d30f6d8a5ae2436c56080138b792dcbce5b
vinyesm <vinyes.marina@gmail.com>
2025-01-08 12:05:48 +0100

    a simple pyspark example

--- examples/pyspark_example.py
+++ examples/pyspark_example.py
@@ -15,15 +15,17 @@ spark = (
 download(
     processes_count=16,
     thread_count=32,
-    url_list="../tests/test_files/test_10000.parquet",
-    output_folder=output_dir,
-    output_format="webdataset",
+    url_list="s3://my-numina/text5B/part-00080-dc7779ff-a280-46f5-8cd8-bf64283145c8-c000.snappy.parquet",
+    output_folder="s3://my-numina/pdf-math-bench,
+    output_format="files",
     input_format="parquet",
-    url_col="URL",
-    caption_col="TEXT",
+    url_col="url",
+    caption_col="alt",
     enable_wandb=True,
     number_sample_per_shard=1000,
     distributor="pyspark",
+    encode_format="pdf",
+    retries=3
 )
 
 # rm -rf bench
